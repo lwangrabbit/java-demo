25c3d01e98a44c083e369935ffe7b6580c05d8eb
10165983 <wang.lipeng3@zte.com.cn>
2018-08-11 16:51:08 +0800

    singleton

--- src/main/java/com/example/demo/Singleton/SingletonExample1.java
+++ src/main/java/com/example/demo/Singleton/SingletonExample1.java
@@ -11,11 +11,15 @@ public class SingletonExample1 {
 
     private volatile static SingletonExample1 instance = null;
 
+    //why volatile, prevent cpu reorder to 1-3-2
+    //1. allocate memory
+    //2. ctor Instance
+    //3. instance = memory
     public static SingletonExample1 getInstance() {
-        if(instance == null) {
+        if(instance == null) {                              //B
             synchronized (SingletonExample1.class) {
                 if(instance == null) {
-                    instance = new SingletonExample1();
+                    instance = new SingletonExample1();     //A-3
                 }
             }
         }
