da0b2ae23e725f88f1a47bdd4c55a9c24724bc83
wanglipeng <wanglipeng.007@gmail.com>
2018-09-09 21:42:33 +0800

    hystrix

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+//import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableCircuitBreaker   //启用熔断器
 public class DemoApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demo/HttpInterceptor.java
+++ src/main/java/com/example/demo/HttpInterceptor.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import com.example.demo.threadlocal.RequestHolder;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.lang.Nullable;
+//import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,7 +18,7 @@ public class HttpInterceptor extends HandlerInterceptorAdapter {
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         RequestHolder.remove();
         log.info("afterCompletion");
     }
